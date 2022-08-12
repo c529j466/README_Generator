bcb84f87d3b176d43e87e2ea6da62cd36b8c6d01
Christian Jones <c529j466@yahoo.com>
2022-08-11 23:21:44 -0500

    Added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Give a description of your project.',
+      name: 'Description',
+    },
+    {
+      type: 'input',
+      message: 'What are the installation instructions?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Do you have information on usage?',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'Do you have any licenses?',
+      name: 'licenses',
+      choices: ['BSD', 'MIT', 'GPL', 'None'],
+    },
+    {
+      type: 'input',
+      message: 'What are the contribution instructions?',
+      name: 'contributions',
+    },
+    {
+      type: 'input',
+      message: 'Do you have any tests for your project?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'Github',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    }
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
